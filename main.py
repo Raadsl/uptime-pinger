ff1b4e76edfaae10e5dbf83f0e223f960db2a263
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-10-14 22:42:18 +0000

    fixed verifying repl

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import requests, replitdb, asyncio
 from server import actualrun
-
+import aiohttp
 
 actualrun()
 
 
 dab = replitdb.AsyncClient()
 
-
+print("All sites in DB:")
 print(asyncio.run(dab.view('pings')))
 
 async def ping():
-  
   an=0
   gn=0
   bn=0
@@ -21,26 +20,39 @@ async def ping():
     if i not in lst:
       try:
         if i != '':
-          requests.get(i)
-          an+=1
-          gn+=1
-          print(f"Pong: {i}")
-      except:
+          timeout = aiohttp.ClientTimeout(total=10)
+          async with aiohttp.ClientSession(timeout=timeout) as session:
+            async with session.get(i) as resp:
+              if resp.status in [200, 304, 100, 201, 202, 206, 302]:
+                an += 1
+                gn += 1
+                bn += 1
+              else:
+                an+=1
+                bn+=1 
+                print(f"Failed: {i} - {resp.status}")
+              print(f"Pong: {i} - {resp.status}")
+          
+          await asyncio.sleep(.05)
+      except Exception as e:
+        print(f"Failed: {i} - timed out probably")
         an+=1
         bn+=1 
         continue
       lst.append(i)
+  with open("pings.txt", "w") as v:
+    v.write(f'{an}\n{gn}\n{bn}')
     
-    with open("pings.txt", "w") as v:
-      v.write(f'{an}\n{gn}\n{bn}')
+
   
       
 
 async def loop():
 
   while True:
-    
+    print("Pinging\n")
     await ping()
+
     print("done with pinging")
     await asyncio.sleep(16)
 
--- server.py
+++ server.py
@@ -3,22 +3,40 @@ from threading import Thread
 import urllib.parse
 import aiohttp
 from aiohttp import web
+import requests
 
 dab = replitdb.AsyncClient()
 app = flask.Flask(__name__)
 
-
-@app.route('/')
+"""
+@app.route('/') #old main page
 def index():
   return flask.render_template('index.html')
+"""
+
+@app.route("/") #new main page with login
+def login():
+  
+  return flask.render_template('login.html',
+        user_id=flask.request.headers['X-Replit-User-Id'],
+        user_name=flask.request.headers['X-Replit-User-Name']
+    )
+
 
 @app.route("/factory")
 def factory():
   return flask.render_template('factory.html')
 
+
+
+
 @app.route("/others")
 def others():
   return flask.render_template('others.html')
+
+@app.route("/ping")
+def ping():
+  return "200" #backup pinger
 
 @app.route("/coolpeeps")
 def pplwhousethis():
@@ -47,7 +65,34 @@ async def remove(url):
   dab.set(pings=str(dab.view('pings')).replace("\n"+url, ""))
 
 
-async def check_replit(url, s=None):
+async def check_replit(url, username, s=None):
+  url = urllib.parse.urlparse(url)
+  host = url.netloc
+  url = f'https://{host}/__repl'
+  own_session = False
+  if not s:
+    timeout = aiohttp.ClientTimeout(total=15)
+    s = aiohttp.ClientSession(timeout=timeout)
+    own_session = True
+
+  try:
+    r = await s.get(url)
+    if own_session: await s.close()
+    if str(r.status)[0] == '4' and not str(r.url).startswith('https://replit.com/'):
+      print(r.status, str(r.url))
+      return False
+    else:
+      return False
+    print(url, r.url, r.status)
+
+  except Exception as e:
+    print(type(e), e, '----- WARNING ')
+    if own_session: await s.close()
+    return True
+
+
+  
+async def check_owner(url, username, s=None):
   url = urllib.parse.urlparse(url)
   host = url.netloc
   url = f'https://{host}/__repl'
@@ -64,16 +109,23 @@ async def check_replit(url, s=None):
       print(r.status, str(r.url))
       return False
     print(url, r.url, r.status)
+    inputuser1 = str(r.url)
+    inputuser1 = inputuser1.replace("https://replit.com/@", "")
+    inputuser, slug = inputuser1.split("/")
+    print(inputuser)
+    
     with open("projects.txt", "a") as f:
       f.write(f"{r.url}\n")
-    return True
+    if username == inputuser:
+      print("check_replit: ", username, "is the owner")
+      return True
+    else:
+      print("check_replit: ", username, "is not the owner")
+      return False
   except Exception as e:
-    print(type(e), e, 'bruh!')
+    print(type(e), e, '----- WARNING ')
     if own_session: await s.close()
     return True
-
-  
-
 
 
 
@@ -85,7 +137,7 @@ def stats():
   replcount = str(len(replcount))
   pingcount, online, offline = content.split('\n')
   try:
-    onlinepercent = int(online) / int(pingcount) * 100
+    onlinepercent = round(int(online) / int(pingcount) * 100)
     onlinepercent = str(onlinepercent).replace('.0', '%')
   except: onlinepercent = 'ERROR WITH CALCULATION! TRY AGAIN LATER. (MY CAPS IS STUCK HELP)'
     
@@ -125,9 +177,14 @@ def send():
   print(str(asyncio.run(dab.view('pings'))))
   print(pings)
   # remove ping
-  is_replit = asyncio.run(check_replit(newPing))
+  if 'X-Replit-User-Name' in flask.request.headers:
+   username = flask.request.headers['X-Replit-User-Name']
+  is_replit = asyncio.run(check_replit(newPing, username))
   if not is_replit:
-    return flask.render_template('msg.html', message="This is not a replit server! (or you are using a proxy). RDSL Uptimer only supports Replit projects!")
+    return flask.render_template('msg.html', message="This is not a replit server, or this isnt a Repl from you!! RDSL Uptimer only supports Replit projects, that were added by there owners!")
+  is_owner = asyncio.run(check_owner(newPing, username))
+  if not is_owner:
+    return flask.render_template('msg.html', message="This isnt a Repl from you!! RDSL Uptimer only pings Repls with permission from their owners!")
   newPing = newPing.lower()
   remPing = newPing.replace("rem ", "", 1)
   if remPing in pings and newPing.startswith("rem "):
@@ -145,7 +202,9 @@ def send():
     if newPing.startswith("http"):
     # ping the newping
       try:
-        requests.get(newPing)
+        reqq = requests.get(newPing)
+        if not reqq.status_code in [200, 304, 100, 201, 202, 206, 302]:
+          return flask.render_template('msg.html', message="Invalid URL! Please make sure you configured the webserver right!")
         if checknames(newPing) >= 25:
           return flask.render_template('msg.html', message="You are already at 25 replits! Thats the max we ping. Also you can't have more then 20 Repls online at the same time on your Replit account, so its useless anyway. You can remove repls by inputting `rem + replurl` to the urlpinger input.")
         asyncio.run(dab.set(pings=str(asyncio.run(dab.view('pings'))) + '\n' + newPing))
@@ -158,8 +217,9 @@ def send():
     return flask.render_template('msg.html', message="I am already pinging that URL!")
 
 
-@app.route('/api/cli', methods=['POST'])
+@app.route('/api/cli', methods=['POST']) #yes ik this is less protected (w\out repl auth)
 def sendcli():
+  
   newPing = flask.request.form['add']
   pings = str(asyncio.run(dab.view('pings'))).split('\n')
   rawpings = str(asyncio.run(dab.view('pings')))
@@ -168,7 +228,7 @@ def sendcli():
   # remove ping
   is_replit = asyncio.run(check_replit(newPing))
   if not is_replit:
-    return "This is not a replit server! (or you are using a proxy). RDSL Uptimer only supports Replit projects!"
+    return "This is not a replit server! RDSL Uptimer only supports Replit projects!"
   newPing = newPing.lower()
   remPing = newPing.replace("rem ", "", 1)
   if remPing in pings and newPing.startswith("rem "):
@@ -185,10 +245,11 @@ def sendcli():
   if newPing not in pings:
     if newPing.startswith("http"):
       try:
+        
         requests.get(newPing)
         asyncio.run(dab.set(pings=str(asyncio.run(dab.view('pings'))) + '\n' + newPing))
         print(f"Added {newPing} to the database via CLI")
-        return "200 - URL successfully Added! consider tipping me at the bottom-right of the site, since you get one dollar for free!"
+        return "200 - URL successfully Added! consider tipping me at https://zink.tips/raadsel, since you get one dollar for free!"
       except:
         return "Invalid URL! Please make sure you configured the webserver right!"
     else:
@@ -199,7 +260,8 @@ def sendcli():
 
 import random
 def run():
-  app.run(host='0.0.0.0', port=random.randint(1000, 9999))
+  from waitress import serve
+  serve(app, host='0.0.0.0', port=random.randint(1000, 9999))
   
 def actualrun():  
     t = Thread(target=run)
